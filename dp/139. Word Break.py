7c3858095013aabc02ba3aa74837a0e30bf8bd6e
Johnson_xie <johnson19921005@163.com>
2020-04-13 02:54:14 +0800

    --no-edit

--- dp/139. Word Break.py
+++ dp/139. Word Break.py
@@ -16,3 +16,36 @@ class Solution:
                         return True
 
         return dp[-1]
+
+"""缓存： 所有有return的地方都要加"""
+class Solution2:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        self.cache = {}
+        self.words = set(wordDict)
+        self.end = len(s)
+
+        def search(s, i):
+
+            if i in self.cache:
+                return self.cache[i]
+
+            # back point
+            if i == self.end:
+                self.cache[i] = True
+                return True
+
+            # process
+            for word in wordDict:
+                if s[i: i + len(word)] in self.words and search(s, i + len(word)):
+                    # dirll down
+                    self.cache[i] = True
+                    return True
+
+            # clear
+            self.cache[i] = False
+            return False
+
+        return search(s, 0)
+
+
